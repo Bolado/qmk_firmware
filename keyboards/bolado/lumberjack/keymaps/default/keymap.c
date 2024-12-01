f825d12f5f495b706fdebc0af3319fc7cd7840fe
Igor <github@bolado.dev>
2024-12-01 19:10:12 +0100

    refactor keymap to add new layers for symbols, navigation, and number functions

--- keyboards/bolado/lumberjack/keymaps/default/keymap.c
+++ keyboards/bolado/lumberjack/keymaps/default/keymap.c
@@ -18,10 +18,14 @@
 
 enum layers {
   _QWERTY = 0,
-  _FUNCTION
+  _SYM,
+  _NAV,
+  _NUM
 };
 
-#define FN MO(_FUNCTION)
+#define SYM MO(_SYM)
+#define NAV MO(_NAV)
+#define NUM MO(_NUM)
 
 // Left-hand home row mods
 #define HOME_S LALT_T(KC_S)
@@ -37,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------. ,-----------------------------------------.
- * |   `  |  1   |  2   |  3   |  4   |  5   | |  6   |  7   |  8   |  9   |  0   | BkSp |
+ * |   `  |  1   |  2   |  3   |  4   |  5   | |  6   |  7   |  8   |  9   |  0   |  Del |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Tab  |  Q   |  W   |  E   |  R   |  T   | |  Y   |  U   |  I   |  O   |  P   | Del  |
+ * | Tab  |  Q   |  W   |  E   |  R   |  T   | |  Y   |  U   |  I   |  O   |  P   | Print|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Esc  |  A   |  S   |  D   |  F   |  G   | |  H   |  J   |  K   |  L   |  ;   |  '   |
+ * | Esc  |  A   |  S   |  D   |  F   |  G   | |  H   |  J   |  K   |  L   |  ;   | Entr |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Shft |  Z   |  X   |  C   |  V   |  B   | |  N   |  M   |  ,   |  .   |  /   | Entr |
+ * | Shft |  Z   |  X   |  C   |  V   |  B   | |  N   |  M   |  ,   |  .   |  /   | Shft |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Ctrl |  Fn  | Alt  | Cmd  |  Fn  | Spac | | Spac |  Fn  | Left | Down |  Up  | Rght |
+ * | Ctrl |  GUI | Alt  | SYM  | Spac | BkSp | | SYM  |  NUM | NAV  | Down |  Up  | Ctrl |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_5x12(
@@ -53,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PSCR,
     KC_CAPS, KC_A,    HOME_S,  HOME_D,  HOME_F,  KC_G,            KC_H,    HOME_J,  HOME_K,  HOME_L,  KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, FN,      KC_SPC,  KC_BSPC,         KC_SPC,  FN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT, SYM,     KC_SPC,  KC_BSPC,         SYM,     NUM,     NAV,     KC_DOWN, KC_UP,   KC_RCTL
 ),
 
 /* Function and numpad
@@ -69,12 +73,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      | |      |      |      |      |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
-[_FUNCTION] = LAYOUT_ortho_5x12(
+[_NUM] = LAYOUT_ortho_5x12(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,           XXXXXXX, KC_7,    KC_8,    KC_9,    KC_ASTR, XXXXXXX,
     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,          XXXXXXX, KC_4,    KC_5,    KC_6,    KC_PLUS, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_MINS, KC_ENT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_0,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+/* Navigation
+ * ,-----------------------------------------. ,-----------------------------------------.
+ * |      |      |      |      |      |      | |      |      | PgUp |      |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      |      |      | |      |      |  Up  |      |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      |      |      | | Home | Left | Down | Right| End  |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      |      |      | |      |      | PgDn |      |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      |      |      | |      |      |      |      |      |      |
+ * `-----------------------------------------' `-----------------------------------------'
+ */
+[_NAV] = LAYOUT_ortho_5x12(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+/* Symbols
+ * ,-----------------------------------------. ,-----------------------------------------.
+ * | BOOT |      |      |      |      |      | |      |      |      |      |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |   `  |   <  |   >  |   @  |   .  | |   !  |   &  |   [  |   ]  |   _  |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |   "  |   -  |   +  |   =  |   #  | |   |  |   :  |   (  |   )  |   %  |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |   '  |   /  |   *  |   ^  |      | |   ~  |   $  |   {  |   }  |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      |      |      | |      |      |      |      |      |      |
+ * `-----------------------------------------' `-----------------------------------------'
+ */
+[_SYM] = LAYOUT_ortho_5x12(
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_GRV,  KC_LT,   KC_GT,   KC_AT,   KC_PDOT,        KC_EXLM, KC_AMPR, KC_LBRC, KC_RBRC, KC_UNDS, XXXXXXX,
+    XXXXXXX, KC_DQUO, KC_PMNS, KC_PPLS, KC_EQL,  KC_HASH,        KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_PERC, XXXXXXX,
+    XXXXXXX, KC_QUOT, KC_PSLS, KC_ASTR, KC_CIRC, XXXXXXX,        KC_TILD, KC_DLR,  KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 };
